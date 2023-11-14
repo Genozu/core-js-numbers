413f03d6395f757b2d479d1ec642f3f1bd8018a5
Nikita Pankov <genozu@yandex.by>
2023-11-14 16:54:33 +0300

    feat: add function contents

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -50,7 +50,7 @@ function getCircleCircumference(radius) {
  *  -3, 3  => 0
  */
 function getAverage(value1, value2) {
-  return (value1 + value2)/2;
+  return (value1 + value2) / 2;
 }
 
 /**
@@ -69,7 +69,7 @@ function getAverage(value1, value2) {
  *   (-5,0) (10,-10) => 18.027756377319946
  */
 function getDistanceBetweenPoints(x1, y1, x2, y2) {
-  return ((x2 - x1)**2 + (y2 - y1)**2)**(1/2);
+  return ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** (1 / 2);
 }
 
 /**
@@ -155,7 +155,7 @@ function parseNumberFromString(value) {
  *   1,2,3   => 3.741657386773941
  */
 function getParallelepipedDiagonal(a, b, c) {
-  return (a ** 2 + b ** 2 + c ** 2) ** (1/2);
+  return (a ** 2 + b ** 2 + c ** 2) ** (1 / 2);
 }
 
 /**
