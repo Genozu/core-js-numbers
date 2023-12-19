c0ada5c73616c36c521993cd7872effeed81d610
Nikita Pankov <genozu@yandex.by>
2023-12-19 13:45:52 +0300

    feat: add functional function toNumber

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -226,8 +226,17 @@ function isPrime(n) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  if (typeof value === 'number') {
+    return value;
+  }
+  if (value instanceof Number) {
+    return value.valueOf();
+  }
+  if (!Number.isNaN(Number(value))) {
+    return Number(value);
+  }
+  return def;
 }
 
 /**
